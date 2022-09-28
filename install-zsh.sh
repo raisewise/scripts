7540bccc8e9aaff02203a65625888f454862d712
raisewise <raisewise@gmail.com>
2022-09-29 01:23:50 +0900

    commit

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
-yum install -y zsh git
-echo /usr/bin/zsh >> /etc/shells
-chsh -s /usr/bin/zsh
-echo ‘Y’ | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' $PWD/.zshrc
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-echo -e 'source .oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source .oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh' >> $PWD/.zshrc
+# OS release
+OS=`cat /etc/os-release | grep ^ID= | awk -F"=" '{print $2}'`
+#echo $OS
 
-sudo apt install -y zsh
+ZSH_CUSTOM=~/.oh-my-zsh/custom
+
+# Package manager
+if [ "$OS" = ubuntu ]
+    PKG=apt
+then
+    PKG=yum
+fi
+#echo $PKG
+
+# oh-my-zsh install
+sudo $PKG install -y zsh git
+sudo echo /usr/bin/zsh >> /etc/shells
 sudo chsh -s /usr/bin/zsh
-sudo echo ‘Y’ | sudo sh -c "$(sudo curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo mv .oh-my-zsh/ /usr/local/src/
+sudo echo ‘Y’ | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' $PWD/.zshrc
+sudo sed -i -e '27 s/# //g' $PWD/.zshrc
+sudo sed -i -e 's/#\ HIST_STAMPS=\"mm\/dd\/yyyy\"/HIST_STAMPS=\"%m\/%d\/%y\ %T"/g' $PWD/.zshrc
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+sudo echo -e 'source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh' >> $PWD/.zshrc
